086ae086e651cec3cc2fe5fed22e8dddf6c8e449
Kamylla Morais <kamyllamoraisviana@hotmail.com>
2023-05-17 19:50:06 -0300

    Swt case

---
+++ SwitchCase e Randon/src/principal/Program.java
@@ -0,0 +1,39 @@
+package principal;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+			/*String opcao = "sexta-feira";
+		
+			
+			switch (opcao) {
+			case "quarta-feira":
+				System.out.println("O dia selecionado é quarta-feira");
+				break;
+			case "quinta-feira":
+				System.out.println("O dia selecionado é quinta-feira");
+				break;
+			case "sexta-feira":
+				System.out.println("sextoooou =P");
+				break;
+				
+				default:
+				System.out.println("Não corresponde a um dia da semana");
+				break;
+			}
+		*/
+		
+		String semana = "quinta";
+		
+		if (semana == "sexta-feira" ){
+		System.out.println("Hoje é sexta!!");
+		}
+		
+		else {
+			System.out.println("Não é sexta");
+			}
+		
+	}
+
+}
