7b44bc2a5901ae24026838050274a757e313e3ea
Kamylla Morais <kamyllamoraisviana@hotmail.com>
2023-05-19 19:43:17 -0300

    Trabalho dia 19/05/2023

--- Números ímpar/src/principal/Program.java
+++ Números ímpar/src/principal/Program.java
@@ -4,15 +4,19 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		for (int contador= 1 ; contador < 25; contador = contador +2) {
-				System.out.println(contador);
+		int soma = 0;
+		for (int contador= 1 ; contador <= 25; contador = contador +2) {
+						
+			System.out.print(contador + "  ");
+			soma += contador;
+			
+		}
+		
+	    System.out.println();
+		System.out.println("O total é: " + soma);
 		
 		}
-		}
-		
-		
-		
+			
 	}
 
 
