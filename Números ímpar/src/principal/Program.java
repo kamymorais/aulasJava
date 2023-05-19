6dc653c526eca7bf06d7bd83e5f5c3ee5d82e83c
Kamylla Morais <kamyllamoraisviana@hotmail.com>
2023-05-19 20:07:16 -0300

    atualização

--- Números ímpar/src/principal/Program.java
+++ Números ímpar/src/principal/Program.java
@@ -6,17 +6,20 @@ public class Program {
 		// TODO Auto-generated method stub
 		int soma = 0;
 		for (int contador= 1 ; contador <= 25; contador = contador +2) {
-						
+					
+			
 			System.out.print(contador + "  ");
 			soma += contador;
 			
-		}
+			//System.out.println(contador);
+						
+			}
 		
 	    System.out.println();
 		System.out.println("O total é: " + soma);
 		
+		// O \n pula uma linha antes de imprimir...
+		
 		}
 			
 	}
-
-
