aa588aecca4b4ce9978c8a9be0d699f969469e61
Kamylla Morais <kamyllamoraisviana@hotmail.com>
2023-05-18 21:52:27 -0300

    número ímpar ou par

---
+++ Estrutura de repetição/src/principal/Program.java
@@ -0,0 +1,24 @@
+package principal;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		// Mais utilizada quando ja sabemos a quantidade de vezes que precisamos repetir os comando. 
+		
+		/*sinataxe for 
+		 *1º: variavel que inicia o contador ;
+		 *2º condição que deve ser aceita;
+		 *3º incremento;		 * 
+		 */
+		
+		
+	for (int contador = 0; contador < 5; contador ++ ) {
+    
+     System.out.println("Valor de contador: " + contador);
+     
+	}
+		
+	}
+
+}
---
+++ Números ímpar/src/principal/Program.java
@@ -0,0 +1,18 @@
+package principal;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int contador= 1 ; contador < 25; contador = contador +2) {
+				System.out.println(contador);
+		
+		}
+		}
+		
+		
+		
+	}
+
+
