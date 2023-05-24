c27eab0efd2c065075c8c35688ef6e299a4f6cce
Kamylla Morais <kamyllamoraisviana@hotmail.com>
2023-05-23 21:14:21 -0300

    Atualização de dados

--- Ultima atividade laço/src/principal/Program.java
+++ Ultima atividade laço/src/principal/Program.java
@@ -18,51 +18,41 @@ public class Program {
 	    num = sc.nextInt();
 	    
 	    switch (tipo){
-	    case '+':
-	    for (int contador = 1 ; contador <11; contador ++) {
-	    System.out.println(num + " + " + contador + " = " + (num+contador));
-	    }
-	    break;
-	    }
+	    	case '+':
+	    		for (int contador = 1 ; contador <11; contador ++) {
+	    			System.out.println(num + " + " + contador + " = " + (num+contador));
+	    		}
+	    	break;
 	    
-	    switch (tipo){
-	    case '-':
-	    for (int contador = num+1 ; contador <= num + 10; contador ++) {
-	    System.out.println(contador + " - " + num + " = " + (contador-num));
-	    }
-	    break;
-	    }
+	    	
+	    	case '-':
+	    		for (int contador = num+1 ; contador <= num + 10; contador ++) {
+	    			System.out.println(contador + " - " + num + " = " + (contador-num));
+	    		}
+	        break;
+	    	    
+	    	case '*':
+	    		for (int contador = 1 ; contador <11; contador ++) {
+	    			System.out.println(num + " x " + contador + " = " + (num*contador));
+	    		}
+	    	break;
 	    
-	    switch (tipo){
-	    case '*':
-	    for (int contador = 1 ; contador <11; contador ++) {
-	    System.out.println(num + " x " + contador + " = " + (num*contador));
-	    }
-	    break;
-	    }
-	    
-	    switch (tipo){
-	    
-	    case '/':
-	    for (double contador = num ; contador <= num*10; contador+= num) {
-	    System.out.println(contador + " / " + num + " = " + (contador/num));
-	    }
-	    break;
+	    	case '/':
+	    		for (double contador = num ; contador <= num*10; contador+= num) {
+	    			System.out.println(contador + " / " + num + " = " + (contador/num));
+	    		}
+	    	break;
 	    
 	    default:
-			System.out.print("Tipo de operação não encontrada!!");
-			break;
-	    }
+	    	System.out.print("Tipo de operação não encontrada!!");
+		break;
 	    
-	 
-	    
-	    sc.close();
-        
+	      
 		}
-		
+	    sc.close();
 	}
 
-
+}
 
 
 
